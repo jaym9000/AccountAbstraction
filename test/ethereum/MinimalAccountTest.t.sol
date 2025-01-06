d521895d06b5264c12534c54eeb56ce46e6304f0
Jean-Marc R-Mahoro <jean_marcc@hotmail.com>
2025-01-05 22:21:54 -0400

    Completed generating unsigned data, getting userOp hash and signing it

--- test/ethereum/MinimalAccountTest.t.sol
+++ test/ethereum/MinimalAccountTest.t.sol
@@ -6,18 +6,24 @@ import {MinimalAccount} from "../../src/ethereum/MinimalAccount.sol";
 import {DeployMinimal} from "../../script/DeployMinimal.s.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {SendPackedUserOp, PackedUserOperation, IEntryPoint} from "../../script/SendPackedUserOp.s.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 /// @title MinimalAccountTest
 /// @author JM
 /// @notice Tests for the MinimalAccount contract
 /// @dev This contract contains unit tests to verify the functionality of the MinimalAccount contract
 contract MinimalAccountTest is Test {
-    HelperConfig helperConfig;  // Configuration helper for deployment
-    MinimalAccount minimalAccount;  // Instance of the MinimalAccount contract
-    ERC20Mock usdc;  // Mock ERC20 token for testing
+    using MessageHashUtils for bytes32;
 
-    address randomuser = makeAddr("randomUser");  // Address for a non-owner user
-    uint256 constant AMOUNT = 1e18;  // Amount to be used in tests
+    HelperConfig helperConfig; // Configuration helper for deployment
+    MinimalAccount minimalAccount; // Instance of the MinimalAccount contract
+    ERC20Mock usdc; // Mock ERC20 token for testing
+    SendPackedUserOp sendPackedUserOp;
+
+    address randomuser = makeAddr("randomUser"); // Address for a non-owner user
+    uint256 constant AMOUNT = 1e18; // Amount to be used in tests
 
     /// @notice Sets up the test environment
     /// @dev Deploys the MinimalAccount and ERC20Mock contracts before each test
@@ -25,6 +31,7 @@ contract MinimalAccountTest is Test {
         DeployMinimal deployMinimal = new DeployMinimal();
         (helperConfig, minimalAccount) = deployMinimal.deployMinimalAccount();
         usdc = new ERC20Mock();
+        sendPackedUserOp = new SendPackedUserOp();
     }
 
     /// @notice Tests that the owner can execute commands
@@ -67,5 +74,45 @@ contract MinimalAccountTest is Test {
             MinimalAccount.MinimalAccount__NotFromEntryPointOrOwner.selector
         );
         minimalAccount.execute(dest, value, functionData);
+    }
+
+    function testRecoverSignedOp() public {
+        // Arrange
+        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(
+            ERC20Mock.mint.selector,
+            address(minimalAccount),
+            AMOUNT
+        );
+        bytes memory executeCallData = abi.encodeWithSelector(
+            MinimalAccount.execute.selector,
+            dest,
+            value,
+            functionData
+        );
+        PackedUserOperation memory packedUserOp = sendPackedUserOp
+            .generatedSignedUserOperation(
+                executeCallData,
+                address(minimalAccount),
+                helperConfig.getConfig()
+            );
+        bytes32 userOperationHash = IEntryPoint(
+            helperConfig.getConfig().entryPoint
+        ).getUserOpHash(packedUserOp);
+        // Act
+        address actualSigner = ECDSA.recover(
+            userOperationHash.toEthSignedMessageHash(),
+            packedUserOp.signature
+        );
+        // Assert
+        assertEq(actualSigner, minimalAccount.owner());
+    }
+
+    function testValidationOfUserOps() public {
+        // Arrange
+        // Act
+        // Assert
     }
 }
