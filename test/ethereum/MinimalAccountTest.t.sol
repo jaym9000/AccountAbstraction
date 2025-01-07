a47602dba9f16e46a4b758a907ca08e5416e8028
Jean-Marc R-Mahoro <jean_marcc@hotmail.com>
2025-01-06 22:04:52 -0400

    Added another test

--- test/ethereum/MinimalAccountTest.t.sol
+++ test/ethereum/MinimalAccountTest.t.sol
@@ -110,9 +110,45 @@ contract MinimalAccountTest is Test {
         assertEq(actualSigner, minimalAccount.owner());
     }
 
+    // 1. Sign user ops
+    // 2. Call validate userop
+    // 3. Assert the return is correct
     function testValidationOfUserOps() public {
         // Arrange
+        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(
+            ERC20Mock.mint.selector,
+            address(minimalAccount),
+            AMOUNT
+        );
+        bytes memory executeCallData = abi.encodeWithSelector(
+            MinimalAccount.execute.selector,
+            dest,
+            value,
+            functionData
+        );
+        PackedUserOperation memory packedUserOp = sendPackedUserOp
+            .generatedSignedUserOperation(
+                executeCallData,
+                address(minimalAccount),
+                helperConfig.getConfig()
+            );
+        bytes32 userOperationHash = IEntryPoint(
+            helperConfig.getConfig().entryPoint
+        ).getUserOpHash(packedUserOp);
+        uint256 missingAccountFunds = 1e18;
         // Act
+        vm.prank(helperConfig.getConfig().entryPoint);
+        uint256 validationData = minimalAccount.validateUserOp(
+            packedUserOp,
+            userOperationHash,
+            missingAccountFunds
+        );
         // Assert
+        assertEq(validationData, 0);
     }
+
+    // function testEntryPointCanExecuteCommands() public {}
 }
